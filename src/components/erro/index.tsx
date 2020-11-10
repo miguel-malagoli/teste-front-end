d2b846ae5a71f5ed75d34fafce44e65656c07ed8
Miguel Malagoli <miguel.g.malagoli@gmail.com>
2020-11-10 12:31:14 -0300

    Mensagem de nenhum resultado

--- src/components/erro/index.tsx
+++ src/components/erro/index.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
-import { textChangeRangeIsUnchanged } from 'typescript';
 
-const Erro = (props: {text: any}) => {
+const Erro = (props: {errorInfo: any, noResults: boolean}) => {
     return (
         <div className="erro">
-            <p className="erro__emote">{props.text.code + ' :('}</p>
-            <p className="erro__texto" dangerouslySetInnerHTML={{__html: props.text.message}}></p>
+            <p className="erro__emote">
+                {props.noResults ?
+                'Nada :('
+                :
+                (props.errorInfo.code + ' :(')}
+            </p>
+            {props.errorInfo ?
+            <p className="erro__texto" dangerouslySetInnerHTML={{__html: props.errorInfo.message}}></p>
+            :
+            <p className="erro__texto">
+                Não encontramos nenhum resultado. Tente novamente com outros termos de pesquisa.
+            </p>}
+            
         </div>
     );
 };
